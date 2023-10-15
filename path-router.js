b26d0255b22ebcd8fc60ba02828b789b34733c29
Corey Farrell <git@cfware.com>
2023-10-15 12:34:29 -0400

    fix!: Update development dependencies

--- path-router.js
+++ path-router.js
@@ -1,37 +1,37 @@
 export const createRouter = defaultCallback => ({
-	_exactRoutes: {},
-	_subRouters: {},
-	_defaultRoute: defaultCallback
+    _exactRoutes: {},
+    _subRouters: {},
+    _defaultRoute: defaultCallback
 });
 
 export const setExactRoute = (router, pathname, callback) => {
-	router._exactRoutes[pathname] = callback;
+    router._exactRoutes[pathname] = callback;
 };
 
 export const setSubRouter = (router, subPath, subRouter) => {
-	router._subRouters[subPath] = subRouter;
+    router._subRouters[subPath] = subRouter;
 };
 
 export const findRoute = (router, pathname) => {
-	if (pathname in router._exactRoutes) {
-		return fullPath => router._exactRoutes[pathname]('', fullPath);
-	}
+    if (pathname in router._exactRoutes) {
+        return fullPath => router._exactRoutes[pathname]('', fullPath);
+    }
 
-	const subrouterMatches = Object.keys(router._subRouters)
-		.filter(p => pathname.startsWith(p))
-		.sort((a, b) => b.length - a.length);
-	for (const subrouterPath of subrouterMatches) {
-		const subrouter = router._subRouters[subrouterPath];
-		const tailPath = pathname.replace(subrouterPath, '');
-		const callback = findRoute(subrouter, tailPath);
-		if (callback) {
-			return callback;
-		}
-	}
+    const subrouterMatches = Object.keys(router._subRouters)
+        .filter(p => pathname.startsWith(p))
+        .sort((a, b) => b.length - a.length);
+    for (const subrouterPath of subrouterMatches) {
+        const subrouter = router._subRouters[subrouterPath];
+        const tailPath = pathname.replace(subrouterPath, '');
+        const callback = findRoute(subrouter, tailPath);
+        if (callback) {
+            return callback;
+        }
+    }
 
-	if (router._defaultRoute) {
-		return fullPath => router._defaultRoute(pathname, fullPath);
-	}
+    if (router._defaultRoute) {
+        return fullPath => router._defaultRoute(pathname, fullPath);
+    }
 };
 
 export const executeRoute = (router, pathname) => findRoute(router, pathname)(pathname);
--- test.js
+++ test.js
@@ -5,100 +5,100 @@ import * as pathRouter from '@cfware/path-router';
 const {createRouter, setExactRoute, setSubRouter, findRoute, executeRoute} = pathRouter;
 
 t.same(Object.keys(pathRouter).sort(), [
-	'createRouter',
-	'setExactRoute',
-	'setSubRouter',
-	'findRoute',
-	'executeRoute'
+    'createRouter',
+    'setExactRoute',
+    'setSubRouter',
+    'findRoute',
+    'executeRoute'
 ].sort());
 
 t.test('empty route returns undefined', async t => {
-	const router = createRouter();
-	t.type(findRoute(router, ''));
-	t.type(findRoute(router, 'page'));
-	t.type(findRoute(router, 'path/page'));
+    const router = createRouter();
+    t.type(findRoute(router, ''));
+    t.type(findRoute(router, 'page'));
+    t.type(findRoute(router, 'path/page'));
 });
 
 t.test('exact route replacement', async t => {
-	const info = {
-		fn1: 0,
-		fn2: 0
-	};
-	const fn1 = () => info.fn1++;
-	const fn2 = () => info.fn2++;
-	const router = createRouter();
+    const info = {
+        fn1: 0,
+        fn2: 0
+    };
+    const fn1 = () => info.fn1++;
+    const fn2 = () => info.fn2++;
+    const router = createRouter();
 
-	setExactRoute(router, 'path', fn1);
-	executeRoute(router, 'path');
-	t.same(info, {fn1: 1, fn2: 0});
+    setExactRoute(router, 'path', fn1);
+    executeRoute(router, 'path');
+    t.same(info, {fn1: 1, fn2: 0});
 
-	setExactRoute(router, 'path', fn2);
-	executeRoute(router, 'path');
-	t.same(info, {fn1: 1, fn2: 1});
+    setExactRoute(router, 'path', fn2);
+    executeRoute(router, 'path');
+    t.same(info, {fn1: 1, fn2: 1});
 });
 
 t.test('default route match', async t => {
-	const info = {
-		hit: 0,
-		fn1: 0
-	};
-	const defaultRoute = () => info.hit++;
-	const fn1 = () => info.fn1++;
-	const router = createRouter(defaultRoute);
-	setExactRoute(router, 'path', fn1);
+    const info = {
+        hit: 0,
+        fn1: 0
+    };
+    const defaultRoute = () => info.hit++;
+    const fn1 = () => info.fn1++;
+    const router = createRouter(defaultRoute);
+    setExactRoute(router, 'path', fn1);
 
-	executeRoute(router, 'path/');
-	t.same(info, {hit: 1, fn1: 0});
+    executeRoute(router, 'path/');
+    t.same(info, {hit: 1, fn1: 0});
 });
 
 t.test('longest subrouter match', async t => {
-	const info = {
-		hit: 0,
-		router1: 0,
-		router2: 0
-	};
-	const router = createRouter(() => info.hit++);
-	setSubRouter(router, 'path/sub', createRouter(() => info.router1++));
-	setSubRouter(router, 'path', createRouter(() => info.router2++));
+    const info = {
+        hit: 0,
+        router1: 0,
+        router2: 0
+    };
+    const router = createRouter(() => info.hit++);
+    setSubRouter(router, 'path/sub', createRouter(() => info.router1++));
+    setSubRouter(router, 'path', createRouter(() => info.router2++));
 
-	executeRoute(router, '');
-	t.same(info, {hit: 1, router1: 0, router2: 0});
+    executeRoute(router, '');
+    t.same(info, {hit: 1, router1: 0, router2: 0});
 
-	executeRoute(router, 'path');
-	t.same(info, {hit: 1, router1: 0, router2: 1});
+    executeRoute(router, 'path');
+    t.same(info, {hit: 1, router1: 0, router2: 1});
 
-	executeRoute(router, 'path/su');
-	t.same(info, {hit: 1, router1: 0, router2: 2});
+    executeRoute(router, 'path/su');
+    t.same(info, {hit: 1, router1: 0, router2: 2});
 
-	executeRoute(router, 'path/sub');
-	t.same(info, {hit: 1, router1: 1, router2: 2});
+    executeRoute(router, 'path/sub');
+    t.same(info, {hit: 1, router1: 1, router2: 2});
 
-	executeRoute(router, 'path/subby');
-	t.same(info, {hit: 1, router1: 2, router2: 2});
+    executeRoute(router, 'path/subby');
+    t.same(info, {hit: 1, router1: 2, router2: 2});
 
-	executeRoute(router, 'unknown');
-	t.same(info, {hit: 2, router1: 2, router2: 2});
+    executeRoute(router, 'unknown');
+    t.same(info, {hit: 2, router1: 2, router2: 2});
 
-	setSubRouter(router, 'broken', createRouter());
-	executeRoute(router, 'broken');
-	t.same(info, {hit: 3, router1: 2, router2: 2});
+    setSubRouter(router, 'broken', createRouter());
+    executeRoute(router, 'broken');
+    t.same(info, {hit: 3, router1: 2, router2: 2});
 });
 
 t.test('fallthrough to shorter subrouter match', async t => {
-	const info = {
-		hit: 0,
-		router: 0
-	};
-	const router = createRouter(() => info.hit++);
-	setSubRouter(router, 'path/sub', createRouter());
-	setSubRouter(router, 'path', createRouter(() => info.router++));
+    const info = {
+        hit: 0,
+        router: 0
+    };
+    const router = createRouter(() => info.hit++);
+    setSubRouter(router, 'path/sub', createRouter());
+    setSubRouter(router, 'path', createRouter(() => info.router++));
 
-	executeRoute(router, 'path/sub');
-	t.same(info, {hit: 0, router: 1});
+    executeRoute(router, 'path/sub');
+    t.same(info, {hit: 0, router: 1});
 
-	executeRoute(router, 'path/sub/sub');
-	t.same(info, {hit: 0, router: 2});
+    executeRoute(router, 'path/sub/sub');
+    t.same(info, {hit: 0, router: 2});
 
-	executeRoute(router, 'path');
-	t.same(info, {hit: 0, router: 3});
+    executeRoute(router, 'path');
+    t.same(info, {hit: 0, router: 3});
 });
